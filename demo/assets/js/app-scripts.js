3608b33128b1aa4839005e464fa3f9a28c24929b
Durga Prasad Sadhanala <thepixeltreat@gmail.com>
2016-01-27 01:16:54 -0700

    updated with comments.

--- demo/assets/js/app-scripts.js
+++ demo/assets/js/app-scripts.js
@@ -1,56 +1,79 @@
-'use strict';
-
 // application namespace to avoid global scope polution, re-use functionality and keep modularity for the code
 var ciscoUI = ciscoUI || {};
 
-
 /**
-* since this is limited for demo my skills I'm adding everything 
-* in this file, but assume this as a nav module independent of 
-* others modules.
-*/
+ * since this is limited for demo my skills I'm adding everything
+ * in this file, but assume this as a nav module independent of
+ * others modules.
+ */
 (function ($, ns) {
-    
+    'use strict';
+
+    // module level variables
+    var rootEle = document.documentElement;
     var moduleEle;
-    
     var loginBtn;
-    
+
+    /**
+     * module initialization
+     */
     var initialize = function () {
         moduleEle = $('[data-uimodule="sitenav"]');
         loginBtn = moduleEle.find('.site-nav__ol__li__login-btn');
-        
+
+        /*
+        * this will remove the no-js class from HTML element to have fallbacks in CSS.
+        * generally this will be handled from global module not here
+        */
+        $(rootEle).removeClass('no-js').addClass('js');
+
         // add events
         _addEvents();
     };
-    
+
+    /**
+     * all modalue related events handled here
+     */
     var _addEvents = function () {
-        loginBtn.on('click', toggleLoginModal);
+        loginBtn.on('click', _toggleLoginModal);
     };
-    
-    var toggleLoginModal = function (e) {
+
+    /**
+     * Show/hide login modal on click of login button
+     * @param  {event object} e
+     */
+    var _toggleLoginModal = function (e) {
         var clickedEle = $(e.target);
-        
-        $('html').toggleClass('has-login-modal-open');
+
+        $(rootEle).toggleClass('has-login-modal-open');
         clickedEle.toggleClass('is-active');
-    }
-    
+    };
+
+    /**
+     * This will clear all module specific variables and events from page
+     */
     var dispose = function () {
         moduleEle.parentNode.removeChild(moduleEle);
         delete ns.sitenav;
         moduleEle = null;
         loginBtn = null;
-        
-        loginBtn.off('click');
 
-        console.log("module removed from page");
+        // remove events
+        loginBtn.off('click');
     };
-    
-    return ns.sitenav = {
+
+    /**
+     * using revealing module pattern so public methods exposed here
+     * @type {Object}
+     */
+    ns.sitenav = {
         initialize: initialize,
         dispose: dispose
     };
-    
+
+    return ns.sitenav;
+
 })(jQuery, ciscoUI || {});
 
-// initialize module, I prefer to do this in a deferent way, but for the demo code using this to keep it simple.
+// initialize module, I prefer to do this in a deferent way, but for the demo code, adding here to keep it simple.
 jQuery(document).ready(ciscoUI.sitenav.initialize);
